2ae6ae0a88b3f2bede35b6b87ce57af2c30b2366
xidwang <36823208+xidwang@users.noreply.github.com>
2018-03-13 15:30:16 -0700

    Add files via upload

--- MUSK.R
+++ MUSK.R
@@ -3,6 +3,15 @@ load("/home/xidwang/A-dong/data_env.RData")
 y_musk=labe
 x_musk=scale_0_1(clean2[,-c(1,2,169)])   # 6598 by 166
 
+lkratio2=function(v)
+{
+  l=length(v)
+  MM=matrix(rep(v,l),l,l)
+  dist=log(sweep(MM,2,v,"/"))
+  dist[is.infinite(dist)]=10
+  dist[is.na(dist)]=0
+  return(dist)
+}
 
 loglkratio_musk=function(v,N)
 {
@@ -21,16 +30,19 @@ loglkratio_musk=function(v,N)
   {
     M[which(steel_cut==j),] =t(matrix(rep( count_musk(y_musk[which(steel_cut==j)]) ,table(steel_cut)[j]),max(y_musk),table(steel_cut)[j]))
   }
-  distance=matrix(0,nrow(x_musk),1)
+  distance=matrix(0,nrow(x_musk),nrow(x_musk))
   for(j in 1:max(y_musk))
-  {distance=pmax(distance,abs(lkratio(M[,j]))) }
+  {distance=pmax(distance,abs(lkratio2(M[,j]))) }   # lkratio2
   return(distance)
 }
 
-final_distance=loglkratio_musk(1,10)
-
-save(final_distance,file="MUSK_distance")
-
-heatmap(final_distance,distfun=as.dist,hclustfun=function(d) hclust(d,method = "ward.D2"),sym=TRUE)
+#final_distance=loglkratio_musk(1,10)
+#save(final_distance,file="MUSK_distance")
 
 
+Q=100
+asso_musk=matrix(0,166,166)
+for(i in 1:166)
+{asso_musk[i,]=apply(x_musk,2, function(x) iono_association(x_musk[,i],x))}
+
+save(asso_musk,file="MUSK_asso")
